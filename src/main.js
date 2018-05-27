2d898103f20bb696884186ee24c28d9a4ce0d157
Fabio Martins <fabiomartinsbr@gmail.com>
2018-05-27 19:55:09 -0300

    Add husky

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const a = 3;
+const b = 2;
+const b1 = 4
+
+const c = 'feijeijeifjiejfeifjefjjjfeijfeijfiejfiejfiejfiejfiejfiejfijeifjeifjeijfeijfiejfiejfiejfiejfiejfiejfiejfijeifjeifjeijfeijfiejf';
+
+
+function sum() {
+  return a + b + b1 + c;
+}
+
+sum();
